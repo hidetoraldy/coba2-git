2df18bd414461d3778d2d64f81896403d3aa9ecc
hidetoraldy <hidetoraldy@gmail.com>
2023-02-10 14:54:07 +0700

    menambahkan paragraf baru

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, velit, quis facere beatae voluptatum sequi provident harum fugiat quos ratione quam illum illo officia ducimus tempore consequatur, ipsum ut quo.</p>
 </body>
 </html>
